d3a72f4e742bf31d00ee3593d2b9c0cec2a6fc7a
torrex <jtorrex@posteo.net>
2022-09-13 00:45:32 +0200

    Added exeptions for errors + parsed JSON

--- download-losing-games.py
+++ download-losing-games.py
@@ -1,48 +1,57 @@
 # https://medium.com/quick-code/absolute-beginners-guide-to-slaying-apis-using-python-7b380dc82236
+# https://pynative.com/parse-json-response-using-python-requests-library/
+
 import requests
 from requests.structures import CaseInsensitiveDict
+from requests.exceptions import HTTPError
 
 headers = CaseInsensitiveDict()
 headers["Accept"] = "application/x-ndjson"
 headers["Content-Type"] = "application/x-ndjson"
 data = '{"Content-type":"application/x-ndjson"}'
 
-white_url = 'https://lichess.org/api/games/user/jtorrex?color=white&rated=true&tags=true&clocks=false&evals=false&opening=false&perfType=blitz&pgnInJson=true'
+white_url = 'https://lichess.org/api/games/user/jtorrex?color=white&rated=true&tags=true&clocks=false&evals=false&opening=false&perfType=blitz&pgnInJson=true&max=1'
 
-print ("Starting! Downloading games played as white")
+try:
+    response_white_games = requests.get(white_url,headers=headers)
+    response_white_games.raise_for_status()
 
-#white_games = requests.get(white_url)
-#print ("Download complete.! Printing games")
-#print(white_games.text)
-#print(white_games.status_code)
+    white_games_json = response_white_games.json()
+    print("White JSON response")
+    #print(white_games_json)
 
-print ("Starting! Downloading JSON games played as white")
-white_games = requests.get(white_url,headers=headers,data=data)
-#white_games_json = white_games.json()
-print ("Download complete.! Printing games")
-print(white_games.text)
-print(white_games.status_code)
-#print(white_games_json)
+    #print("Print each key-value pair from JSON response")
+    #for key, value in white_games_json.items():
+    #    print(key, ":", value)
 
-#for p in white_games['winner']:
-#    print(p['status'])
+    print("Printing atomic keys from white games JSON")
+    print(white_games_json["winner"])
+    print(white_games_json["pgn"])
 
-black_url = 'https://lichess.org/api/games/user/jtorrex?color=black&rated=true&tags=true&clocks=false&evals=false&opening=false&perfType=blitz&pgnInJson=true'
+except HTTPError as http_err:
+    print(f'HTTP error occurred: {http_err}')
+except Exception as err:
+    print(f'Other error occurred: {err}')
 
-print ("Starting! Downloading games played as black")
+black_url = 'https://lichess.org/api/games/user/jtorrex?color=black&rated=true&tags=true&clocks=false&evals=false&opening=false&perfType=blitz&pgnInJson=true&max=1'
 
-#black_games = requests.get(black_url)
-#print ("Download complete.! Printing games")
-#print(black_games.text)
-#print(black_games.status_code)
+try:
+    response_black_games = requests.get(black_url,headers=headers)
+    response_black_games.raise_for_status()
 
-print ("Starting! Downloading JSON games played as black")
-black_games = requests.get(black_url,headers=headers,data=data)
-#black_games_json = black_games.json()
-print ("Download complete.! Printing games")
-print(black_games.text)
-print(black_games.status_code)
+    black_games_json = response_black_games.json()
+    print("Black JSON response")
+    #print(black_games_json)
 
-#for p in black_games['winner']:
-#    print(p['status'])
-#print(black_moves_json)
+    #print("Print each key-value pair from JSON response:")
+    #for key, value in black_games_json.items():
+    #    print(key, ":", value)
+
+    print("Printing atomic keys from black games JSON:")
+    print(black_games_json["winner"])
+    print(black_games_json["pgn"])
+
+except HTTPError as http_err:
+    print(f'HTTP error occurred: {http_err}')
+except Exception as err:
+    print(f'Other error occurred: {err}')
