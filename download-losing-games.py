f83163f0c8778b55d7718f950aaa0311444d4e41
torrex <jtorrex@posteo.net>
2022-10-05 01:41:13 +0200

    Removed limits and conditional section to filter by results

--- download-losing-games.py
+++ download-losing-games.py
@@ -12,15 +12,18 @@ headers["Accept"] = "application/x-ndjson"
 headers["Content-Type"] = "application/x-ndjson"
 data = '{"Content-type":"application/x-ndjson"}'
 
+print("=== Lichess Losing Games Exporter ===\n")
+
 try:
 
-    white_url = 'https://lichess.org/api/games/user/jtorrex?color=white&rated=true&tags=true&clocks=false&evals=false&opening=false&perfType=blitz&pgnInJson=true&max=2'
+    print("- Downloading White Games...\n")
+    white_url = 'https://lichess.org/api/games/user/jtorrex?color=white&rated=true&tags=true&clocks=false&evals=false&opening=false&perfType=blitz&pgnInJson=true'
 
     # Make the request to the Lichess API
     response_white_games = requests.get(white_url,headers=headers)
     response_white_games.raise_for_status()
 
-    # Store the result of the request as a file
+    # Store the result of the request as a JSON file
     with open("white_games.json", "w") as file:
     	file.write(response_white_games.text)
     	file.close()
@@ -33,29 +36,42 @@ try:
             white_games_dict = json.loads(json_game)
             white_games_list.append(white_games_dict)
 
-    # Print each JSON decoded Object
+    # Print each JSON decoded Object (game)
     for game in white_games_list:
-        if game["winner"] == "black":
-            print("https://lichess.org/"+game["id"]+"\n")
+        if game["status"] == "draw":
+            game = "https://lichess.org/"+game["id"]
+            print(game + " - Draw")
+            with open("white_drawing_games_urls.out", "w") as file:
+                file.write(game)
 
+        elif game["winner"] == "black":
+            game = "https://lichess.org/"+game["id"]
+            print(game + " - Lose")
+            with open("white_losing_games_urls.out", "w") as file:
+                file.write(game)
         else:
-            print("https://lichess.org/"+game["id"]+"\n")
+            game = "https://lichess.org/"+game["id"]
+            print(game + " - Win")
+            with open("white_winning_games_urls.out", "w") as file:
+                file.write(game)
+
+    print("- Done!\n")
 
 except HTTPError as http_err:
     print(f'HTTP error occurred: {http_err}')
 except Exception as err:
     print(f'Other error occurred: {err}')
 
-
 try:
 
-    black_url = 'https://lichess.org/api/games/user/jtorrex?color=black&rated=true&tags=true&clocks=false&evals=false&opening=false&perfType=blitz&pgnInJson=true&max=2'
+    print("- Downloading Black Games...\n")
+    black_url = 'https://lichess.org/api/games/user/jtorrex?color=black&rated=true&tags=true&clocks=false&evals=false&opening=false&perfType=blitz&pgnInJson=true'
 
     # Make the request to the Lichess API
     response_black_games = requests.get(black_url,headers=headers)
     response_black_games.raise_for_status()
 
-    # Store the result of the request as a file
+    # Store the result of the request as a JSON file
     with open("black_games.json", "w") as file:
     	file.write(response_black_games.text)
     	file.close()
@@ -70,10 +86,20 @@ try:
 
     # Print each JSON decoded Object
     for game in black_games_list:
+
         if game["winner"] == "white":
-            print("https://lichess.org/"+game["id"]+"\n")
+            game = "https://lichess.org/"+game["id"]
+            print(game + " - Lose")
+            with open("black_losing_games_urls.out", "w") as file:
+                file.write(game)
+        
         else:
-            print("https://lichess.org/"+game["id"]+"\n")
+            game = "https://lichess.org/"+game["id"]
+            print(game + " - Win")
+            with open("black_winning_draw_games_urls.out", "w") as file:
+                file.write(game)
+
+    print("- Done!\n")
 
 except HTTPError as http_err:
     print(f'HTTP error occurred: {http_err}')
